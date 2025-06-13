55f7dd3c8d7258c59cc3e21c1a2fb9ada5b8f7b0
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2025-06-12 20:16:51 -0400

    Edit Config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/manaone23/',
+  base: '/hades2/',
   plugins: [react(),
   tailwindcss(),
   ],
